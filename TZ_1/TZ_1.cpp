f0a03182b4035cac58f098dde6c5014e88339e90
4elovel <marcynakvova007@gmail.com>
2022-10-20 17:57:16 +0300

    Добавьте файлы проекта.

---
+++ TZ_1/TZ_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> solution(vector <int> vec) {
+    try
+    {
+        sort(vec.begin(), vec.end());
+        for (auto i = 0; i < vec.size(); i++)
+        {
+            if (vec.at(i) == vec.at(i + 1))
+            {
+                vec.erase(vec.begin() + i);
+                vec.erase(vec.begin() + i);
+                i--;
+            }
+
+        }
+    }
+    catch (const std::exception&)
+    {
+        return vec;
+    }
+
+    return vec;
+}
+
+int main()
+{
+    vector <int> vec{2,4,6,2,5,4,1,9,8,8,1};
+
+    vector <int> vec_sol = solution(vec);
+
+    for (size_t i = 0; i < vec_sol.size(); i++)
+    {
+        cout << vec_sol[i] << " ";
+    }
+
+}
